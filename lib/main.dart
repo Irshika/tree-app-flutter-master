36830be700562800898f094971de8fd75c9bfcf5
ASUS® <it17387804@my.sliit.lk>
2020-04-24 22:47:58 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:treeapp/models/user.dart';
+import 'package:treeapp/screens/wrapper.dart';
+import 'package:treeapp/services/auth.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
+
